cd551f013b1f254250c99762a96ab13f5cae5005
felipe uribe guevra <https://bitbucket.org/FelipeUsa/workspace/repositories>
2020-12-26 03:32:50 -0500

    Se termina el repaso de HTML

---
+++ Aprendiendo/JavaScript/Variables, sintaxis y funciones/valores.js
@@ -0,0 +1,19 @@
+ //Numericos
+ 40
+
+ //Strings
+ "Jose de Maria"
+
+ //Booleanos
+ true
+ false
+
+//Valores vacios
+ null
+ undefined
+
+ //Array (Tipo objeto)
+ {1, 2, 3}
+
+ //Valot tipo objetoS
+ {nombre: "Diego" }
---
+++ Aprendiendo/JavaScript/Variables, sintaxis y funciones/variables.js
@@ -0,0 +1,17 @@
+var nombre = "Oscar";
+
+//Declarar
+var edad;
+edad
+
+//Inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadors", "Celular"];
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
